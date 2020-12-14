44bafbed67682ef085d43c744168d4738fa20dd3
Multivalence <47169252+Multivalence@users.noreply.github.com>
2020-12-14 16:02:47 -0500

    Update enforceTH.py

--- enforceTH.py
+++ enforceTH.py
@@ -6,14 +6,27 @@ def enforceType(func):
 	def wrapper(*args):
 			wrapper.has_been_called = True
 			x = func.__annotations__
+			print(x)
 			t = [x[i] for i in x if i != 'return']
 
 			if len(args) != len(t):
-				raise TypeError("Missing required positional arguments.")
+				raise TypeError("Missing required positional arguments and/or annotations.")
 
 			for i in range(len(t)):
 				if not isinstance(args[i],t[i]):
 					raise ValueError(f"Invalid literal for {t[i]}: {args[i]}")
-			return func(*args)
+			
+			try:
+				ReturnValue = x['return']
+			
+			except KeyError:
+				raise TypeError("Missing required return value annotation.")
+			
+			RV = func(*args)
+
+			if not isinstance(RV, ReturnValue):
+				raise TypeError(f"Expected function to return {ReturnValue}. Got {type(RV)} instead.")
+
+			return RV
 	wrapper.has_been_called = False
 	return wrapper
