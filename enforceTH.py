474d19e0f17efbcaa401555234ba4a30b5555520
Multivalence <47169252+Multivalence@users.noreply.github.com>
2020-12-14 18:27:21 -0500

    Update enforceTH.py

--- enforceTH.py
+++ enforceTH.py
@@ -1,33 +1,38 @@
 import functools
 
-def enforceType(returnValue : bool=True):
-	def decorator(func):
-		@functools.wraps(func)
-		def wrapper(*args):
-				wrapper.has_been_called = True
-				x = func.__annotations__
-				t = [x[i] for i in x if i != 'return']
 
-				if len(args) != len(t):
-					raise TypeError("Missing required positional arguments and/or annotations.")
+def enforceType(func):
+	@functools.wraps(func)
+	def wrapper(*args):
+			wrapper.has_been_called = True
+			x = func.__annotations__
+			t = [x[i] for i in x if i != 'return']
 
-				for i in range(len(t)):
-					if not isinstance(args[i],t[i]):
-						raise ValueError(f"Invalid literal for {t[i]}: {args[i]}")
-				
-				if returnValue:
-					try:
-						ReturnValue = x['return']
-					
-					except KeyError:
-						raise TypeError("Missing required return value annotation.")
-					
-					RV = func(*args)
+			if len(args) != len(t):
+				raise TypeError("Missing required positional arguments and/or annotations.")
 
-					if not isinstance(RV, ReturnValue):
-						raise TypeError(f"Expected function to return {ReturnValue}. Got {type(RV)} instead.")
+			for i in range(len(t)):
+				if not isinstance(args[i],t[i]):
+					raise ValueError(f"Invalid literal for {t[i]}: {args[i]}")
+			
+			
+			try:
+				ReturnValue = x['return']
+			
+			except KeyError:
+				raise TypeError("Missing required return value annotation.")
+			
+			try:
+				RV = func(*args)
+			
+			except Exception as e:
+				raise Exception(e)
+			
 
-				return func(*args)
-		wrapper.has_been_called = False
-		return wrapper
-	return decorator
+			if not isinstance(RV, ReturnValue):
+				raise TypeError(f"Expected function to return {ReturnValue}. Got {type(RV)} instead.")
+			
+			return RV
+
+	wrapper.has_been_called = False
+	return wrapper
